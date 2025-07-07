734316bd67cb59ac725fd89e1e5903f4014382c6
lpdswing <lpdswing@qq.com>
2025-07-07 15:51:23 +0800

    build: fronted

--- frontend/dist/assets/jszip.min-CVaBfHD0.js
+++ frontend/dist/assets/jszip.min-CVaBfHD0.js
@@ -1,4 +1,4 @@
-import{C as bt}from"./index-6mw23_9c.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
+import{C as bt}from"./index-Cwtstl6k.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module 'markdown-it-katex' {
+    import MarkdownIt from 'markdown-it'
+    const markdownItKatex: MarkdownIt.PluginSimple
+    export default markdownItKatex
+}
